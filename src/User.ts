0c36ffc89bfae495b993514a5690d0689ab2e7e3
abimael <abimaelst@gmail.com>
2022-04-10 19:12:20 -0300

    add popups

--- src/User.ts
+++ src/User.ts
@@ -8,10 +8,14 @@ export class User {
   };
 
   constructor() {
-    this.name = faker.name.firstName()
+    this.name = faker.name.firstName();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude())
-    }
+    };
+  }
+
+  markerContent(): string {
+    return `User Name: ${this.name}`
   }
 }
